9f8861cc8e3d3137909c15502405f7043d4e7a48
Matt Stone <matt@mattstone.com.au>
2022-05-18 11:09:18 +1000

    Remove duplicate helper

--- src/Enpress/Foundation/helpers.php
+++ src/Enpress/Foundation/helpers.php
@@ -1,23 +1,5 @@
 <?php
 
-if (! function_exists('getWordpressPrefix')) {
-
-    /**
-     * Get the database prefix for Wordpress
-     *
-     * @return string
-     * @throws \Illuminate\Container\EntryNotFoundException
-     */
-    function getWordpressPrefix() {
-        $config = app('config');
-        $globalPrefix = $config->get('database.connections.mysql.prefix');
-        $cmsPrefix = $config->get('cms.db_prefix');
-
-        return $globalPrefix . $cmsPrefix;
-    }
-
-}
-
 if (! function_exists('___')) {
     /**
      * Translate the given message.
